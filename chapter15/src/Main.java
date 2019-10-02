e9f6076882e8848ef05b249fc5ccbbf598189440
kiomnd2 <kiomnd2@gmail.com>
2019-10-03 02:00:31 +0900

    chapter15. composite 패턴 , visitor 패턴...

---
+++ chapter15/src/Main.java
@@ -0,0 +1,31 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CompositeTask root = new CompositeTask("Root", LocalDateTime.now());
+        root.addTask("sub1", LocalDateTime.now());
+        root.addTask("sub2", LocalDateTime.now());
+
+        TaskReport report = root.getReport(CompositeSortType.TITLE_ASC);
+        List<TaskReport> list = report.getList();
+        CompositeTask sub1 = list.get(0).getTask();
+        CompositeTask sub2 = list.get(1).getTask();
+        sub1.addTask("sub1-1", LocalDateTime.now());
+        sub1.addTask("sub1-2", LocalDateTime.now());
+        sub2.addTask("sub2-1", LocalDateTime.now());
+        sub2.addTask("sub2-2", LocalDateTime.now());
+
+        /*
+
+        Renderer renderer1 = new Renderer(() -> new ConsoleVisitor());
+        renderer1.render(root.getReport(CompositeSortType.TITLE_ASC));
+*/
+
+
+        Renderer renderer2 = new Renderer(() -> new JsonVisitor());
+        renderer2.render(root.getReport(CompositeSortType.TITLE_ASC));
+
+    }
+}
---
+++ chapter15/src/Visitor.java
@@ -0,0 +1,6 @@
+public interface Visitor {
+    void drawTask(CompositeTask task, int depth);
+
+    void end(int depth);
+
+}
