1b03edc9c7c41b4bb3ca223a319d5cb7ed94f0d7
김형익 <kiomnd2@naver.com>
2019-10-17 23:53:09 +0900

    chapter2. 영화예약 프로그램

---
+++ chapter2/src/Reservation.java
@@ -0,0 +1,15 @@
+public class Reservation {
+    private Customer customer;
+    private Screening Screening;
+    private Money fee;
+    private int audienceCount;
+
+    public Reservation(Customer customer, Screening screening, Money fee, int audienceCount) {
+        this.customer = customer;
+        this.Screening = screening;
+        this.fee = fee;
+        this.audienceCount = audienceCount;
+    }
+
+
+}
